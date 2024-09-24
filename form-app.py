ecd6d765b665a2681065b5f800a2a84e0db2b0b8
danyewest97 <dane20052008@gmail.com>
2024-09-24 13:56:41 -0700

    quick commit

--- form-app.py
+++ form-app.py
@@ -3,20 +3,25 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
-def render_main():
+def render_home():
     return render_template('home.html')
-
-@app.route("/p1")
-def render_page1():
-    return render_template('page1.html')
-
-@app.route("/p2")
-def render_page2():
-    return render_template('page2.html')
     
-@app.route("/p3")
-def render_page3():
-    return render_template('page3.html')
+@app.route("/short")
+def render_short():
+    return render_template('short.html')
     
+@app.route("/long")
+def render_long():
+    return render_template('long.html')
+    
+@app.route("/random")
+def render_random():
+    return render_template('random.html')
+
+@app.route("/short.py")
+def render_random():
+    return render_template('random.html')
+
+
 if __name__=="__main__":
     app.run(debug=False)
