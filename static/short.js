04aade05b77053cdb7d3ea696781c350ca08ab17
danyewest97 <dane20052008@gmail.com>
2024-09-25 14:01:10 -0700

    quick commit

--- static/short.js
+++ static/short.js
@@ -1,7 +1,40 @@
 $(document).ready(function() {
-	console.log("hi");
-	var animal = $(".short-submit").dataset.animal;
-	// var word = $(".short-submit").dataset.word;
-	$("h1").text(animal);
-	// console.log($(".short-submit").dataset.animal);
+	
+	var animal = $(".short-submit").data("animal");
+	var word = $(".short-submit").data("word");
+	
+	var randomNum = parseInt(Math.random() * 100);
+	var specialCharacter;
+	
+	const specialCharacters = "!?#";
+	const specialArray = specialCharacters.split("");
+	
+	var specialNum = parseInt(Math.random() * specialArray.length);
+	specialCharacter = specialArray[specialNum];
+	
+	const resultArray = [specialCharacter, animal, word, randomNum];
+	shuffleArray(resultArray);
+	
+	
+	var result = "";
+	for (let i = 0; i < resultArray.length; i++) {
+		result += resultArray[i];
+	}
+	
+	$(".result").text(result);
+	
+	
+	
+	
+	function shuffleArray(array) {
+		for (var i = array.length - 1; i >= 0; i--) {
+			// generate random number between 0 and i
+			var j = Math.floor(Math.random() * (i + 1));	
+			
+			// swap the array at the current index with the array at the index of the random number generated
+			var temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	}
 });
