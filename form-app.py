59751d87c537f1c722c61391c40f92d0ef3b8f14
danyewest97 <dane20052008@gmail.com>
2024-09-25 13:08:44 -0700

    quick commit

--- form-app.py
+++ form-app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -18,9 +18,9 @@ def render_long():
 def render_random():
     return render_template('random.html')
 
-@app.route("/short.py")
-def render_random():
-    return render_template('random.html')
+@app.route("/short-submit", methods=['GET', 'POST'])
+def render_short_submit():
+    return render_template('short-submit.html', animal=request.form['animal'], word=request.form['word'])
 
 
 if __name__=="__main__":
