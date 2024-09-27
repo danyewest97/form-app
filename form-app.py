d9fc07e0039527a6c0632a01ed21b945779d339d
danyewest97 <dane20052008@gmail.com>
2024-09-27 13:57:38 -0700

    quick commit

--- form-app.py
+++ form-app.py
@@ -3,34 +3,6 @@ import random
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-@app.route("/")
-def render_home():
-    return render_template('home.html')
-    
-@app.route("/short")
-def render_short():
-    return render_template('short.html')
-    
-@app.route("/long")
-def render_long():
-    return render_template('long.html')
-    
-@app.route("/random")
-def render_random():
-    return render_template('random.html')
-
-@app.route("/short-submit", methods=['GET', 'POST'])
-def render_short_submit():
-    animal = request.form['animal']
-    word = request.form['word']
-    return render_template('short-submit.html', password=shortPass(animal, word))
-
-
-if __name__=="__main__":
-    app.run(debug=False)
-
-
-
 def shortPass(animal, word):
     # Empty variable to add on to to create the final password
     result = "";
@@ -65,3 +37,32 @@ def shortPass(animal, word):
         result += value
     
     return result
+
+@app.route("/")
+def render_home():
+    return render_template('home.html')
+    
+@app.route("/short")
+def render_short():
+    return render_template('short.html')
+    
+@app.route("/long")
+def render_long():
+    return render_template('long.html')
+    
+@app.route("/random")
+def render_random():
+    return render_template('random.html')
+
+@app.route("/short-submit", methods=['GET', 'POST'])
+def render_short_submit():
+    animal = request.args['animal']
+    word = request.args['word']
+    return render_template('short-submit.html', password=shortPass(animal, word))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
