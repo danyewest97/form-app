782323590d258b4abaa47c2f925c2b6a04426574
danyewest97 <dane20052008@gmail.com>
2024-09-27 13:53:18 -0700

    quick commit

--- form-app.py
+++ form-app.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for, render_template, request
+import random
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -20,8 +21,47 @@ def render_random():
 
 @app.route("/short-submit", methods=['GET', 'POST'])
 def render_short_submit():
-    return render_template('short-submit.html', animal=request.form['animal'], word=request.form['word'])
+    animal = request.form['animal']
+    word = request.form['word']
+    return render_template('short-submit.html', password=shortPass(animal, word))
 
 
 if __name__=="__main__":
     app.run(debug=False)
+
+
+
+def shortPass(animal, word):
+    # Empty variable to add on to to create the final password
+    result = "";
+    
+    # Array to contain the different parts of the password, gets shuffled before being turned into a string and stored in result
+    resultArray = []
+    
+    # Special characters that can be used in the password, must be separated by spaces
+    specialCharacters = "! ? #";
+    
+    # Splits the special characters string into an array
+    specialArray = specialCharacters.split();
+    
+    # Choosing a random special character to add to the password and adding it to a variable
+    random.shuffle(specialArray)
+    specialCharacter = specialArray[0]
+    
+    # Generating a random number to use in the password
+    randomNum = int(random.random() * 100)
+    
+    # Adding all the different parts of the password to the resultArray
+    resultArray.append(specialCharacter)
+    resultArray.append(animal)
+    resultArray.append(word)
+    resultArray.append(str(randomNum))
+    
+    # Shuffling the result array
+    random.shuffle(resultArray)
+    
+    # Storing the resultArray as one string in result, instead of as an array, before returning the final password
+    for value in resultArray:
+        result += value
+    
+    return result
